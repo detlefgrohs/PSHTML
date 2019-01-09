38c6f6ddff7bfb1cd69f8f5ccc0f58b86b35c095
Unknown <detlef.grohs@gmail.com>
2019-01-08 18:54:47 -0600

    Added code tag.

---
+++ PSHTML/Functions/Public/code.ps1
@@ -0,0 +1,36 @@
+Function code {
+    <#
+    .SYNOPSIS
+    Create a code tag in an HTML document.
+    .LINK
+        https://github.com/Stephanevg/PSHTML
+    #>
+    [Cmdletbinding()]
+    Param(
+        [Parameter(Mandatory = $true)]
+        [AllowEmptyString()]
+        [AllowNull()]
+        $Content,
+
+        [AllowEmptyString()]
+        [AllowNull()]
+        [String]$Class = "",
+
+        [String]$Id,
+
+        [AllowEmptyString()]
+        [AllowNull()]
+        [String]$Style,
+
+        [String]$title,
+
+        [Hashtable]$Attributes
+    )
+
+    Process {
+        $tagname = "code"
+
+        Set-HtmlTag -TagName $tagname -Parameters $PSBoundParameters -TagType nonVoid
+        
+    }
+}
